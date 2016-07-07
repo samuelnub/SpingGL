2051448e1d7dd869d7ff50ae0edca4946ff90a77
samuelnub <notsamnub@gmail.com>
2016-07-07 10:04:33 +0800

    defaults to call incase you cant read the desired file

---
+++ Sping/Sping/headers/globals/xmldefaults.h
@@ -0,0 +1,29 @@
+#pragma once
+#ifndef GLOBALS_XMLDEFAULTS_H
+#define GLOBALS_XMLDEFAULTS_H
+
+namespace XMLDefaults
+{
+	const char *defaultSettings =
+	{
+		"<root>"
+		"	<WindowSettings>"
+		"		<glMajor>4</glMajor>"
+		"		<glMinor>3</glMinor>"
+		"		"
+		"		<depthBits>24</depthBits>"
+		"		<stencilBits>8</stencilBits>"
+		"		<doubleBuffer>1</doubleBuffer>"
+		"		<multisamples>8</multisamples>"
+		"		<name>Sping!</name>"
+		"		"
+		"		<width>1280</width>"
+		"		<height>720</height>"
+		"		<fullscreen>0</fullscreen>"
+		"	</WindowSettings>"
+		"</root>"
+	};
+
+}
+
+#endif
