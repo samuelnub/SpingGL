40bf4040c51d193b8ecb67d26cce80fe3fe46227
samuelnub <notsamnub@gmail.com>
2016-07-14 10:13:39 +0100

    settings test

--- Sping/Sping/source/game/game.cpp
+++ Sping/Sping/source/game/game.cpp
@@ -4,8 +4,8 @@
 
 Game::Game()
 {
-	//setup other things, gui, fonts, etc.
-	window.setup();
+	settings.setup();
+	window.setup(this);
 	states.setup();
 	input.setup(this);
 	commands.setup(this);
@@ -14,19 +14,29 @@ Game::Game()
 	shaders.setup();
 	textures.setup();
 
-	eyes.setup();
 	scene.setup(this);
 
+	//TODO: stupid test stuff lol
+	textures.store("you_tried", "res/textures/test.jpg", "texture_diffuse");
+
 	std::string testText = "test";
+	std::string testTextTwo = "you_tried";
 	std::vector<std::string> _shadeTest;
 	_shadeTest.push_back(testText);
 	std::map<std::string, std::vector<std::string>> _templol;
 	_templol[testText].push_back(testText);
+	std::map<std::string, std::vector<std::string>> _templolTwo;
+	_templolTwo[testText].push_back(testTextTwo);
 
 	glm::mat4 testMat;
 	testMat = glm::translate(testMat, glm::vec3(0.0f, 0.0f, 0.0f));
 	testoRendo.create(69, testMat, &this->meshes, &this->shaders, &this->textures, _shadeTest, _templol);
+
+	testoRendoTwo.create(42, testMat, &this->meshes, &this->shaders, &this->textures, _shadeTest, _templolTwo);
 	scene.stage(&testoRendo);
+	scene.stage(&testoRendoTwo);
+
+	camera.setup(glm::radians(60.0f), 1280.0f / 720.0f, 0.001f, 1000000.0f, glm::vec3(0.0f, 0.0f, 1.0f));
 
 	while (true)
 		this->loop();
